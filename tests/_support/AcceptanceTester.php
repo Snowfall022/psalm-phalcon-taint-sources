93f790242374d178d3e565047e4496aa9deb32be
dier <diyor024@gmail.com>
2021-07-16 12:38:06 +0300

    initial commit

---
+++ tests/_support/AcceptanceTester.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Weirdan\PsalmPluginSkeleton\Tests;
+
+use Weirdan\PsalmPluginSkeleton\Tests\_generated\AcceptanceTesterActions;
+
+/**
+ * Inherited Methods
+ * @method static wantToTest($text)
+ * @method static wantTo($text)
+ * @method static execute($callable)
+ * @method static expectTo($prediction)
+ * @method static expect($prediction)
+ * @method static amGoingTo($argumentation)
+ * @method static am($role)
+ * @method static lookForwardTo($achieveValue)
+ * @method static comment($description)
+ * @method static pause()
+ *
+ * @SuppressWarnings(PHPMD)
+*/
+class AcceptanceTester extends \Codeception\Actor
+{
+    use AcceptanceTesterActions;
+    /**
+     * Define custom actions here
+     */
+}
